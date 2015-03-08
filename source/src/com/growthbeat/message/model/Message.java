894d48e839f3530a77d1150679bb3c6a3bf44a3e
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-03-08 22:07:40 +0900

    Add Message model

---
+++ source/src/com/growthbeat/message/model/Message.java
@@ -0,0 +1,73 @@
+package com.growthbeat.message.model;
+
+import java.util.Date;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.growthbeat.model.Model;
+import com.growthbeat.utils.DateUtils;
+import com.growthbeat.utils.JSONObjectUtils;
+
+public class Message extends Model {
+
+	private String id;
+
+	private Date created;
+
+	public Message() {
+		super();
+	}
+
+	private Message(JSONObject jsonObject) {
+		setJsonObject(jsonObject);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+	@Override
+	public JSONObject getJsonObject() {
+
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("id", getId());
+			jsonObject.put("created", DateUtils.formatToDateTimeString(created));
+		} catch (JSONException e) {
+			return null;
+		}
+
+		return jsonObject;
+
+	}
+
+	@Override
+	public void setJsonObject(JSONObject jsonObject) {
+
+		if (jsonObject == null)
+			return;
+
+		try {
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "id"))
+				setId(jsonObject.getString("id"));
+			if (JSONObjectUtils.hasAndIsNotNull(jsonObject, "created"))
+				setCreated(DateUtils.parseFromDateTimeString(jsonObject.getString("created")));
+		} catch (JSONException e) {
+			throw new IllegalArgumentException("Failed to parse JSON.");
+		}
+
+	}
+}
